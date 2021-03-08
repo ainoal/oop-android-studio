933bcaa47493d0e17a3c089d5e94b22d7a4a6ab8
ainoal <77739788+ainoal@users.noreply.github.com>
2021-03-08 20:04:17 +0200

    vko 7 t 1-4

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,53 +1,5 @@
 package com.example.helloworld;
 
-/*import android.app.Activity;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.widget.EditText;
-import android.widget.TextView;
-
-public class MainActivity extends Activity {
-
-    EditText edittext;
-    TextView text;
-    String Value;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        edittext = (EditText)findViewById(R.id.editText1);
-        text= (TextView)findViewById(R.id.textView1);
-
-        edittext.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // TODO Auto-generated method stub
-
-                Value = edittext.getText().toString();
-
-                text.setText(Value);
-
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                // TODO Auto-generated method stub
-
-            }
-        });
-    }
-} */
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Tästä reposta löytyvät viikkotehtävät viikosta 7 alkaen.
 
 # Viikko 7
-* t. 1-4: ./HelloWorld
+* t. 1-4: 
+    HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+    HelloWorld/app/src/main/res/layout/activity_main.xml
