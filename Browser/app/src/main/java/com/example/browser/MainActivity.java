3e38851a2a0a621a588afc5a3c460b2cc1b89cd2
ainoal <77739788+ainoal@users.noreply.github.com>
2021-04-06 18:40:13 +0300

    vko 11 t 1

--- Browser/app/src/main/java/com/example/browser/MainActivity.java
+++ Browser/app/src/main/java/com/example/browser/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.browser;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,12 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
     EditText url;
     WebView web;
-
+    Button gotoButton, refresh;
+    String currentSite == null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("moi");
 
         url = (EditText) findViewById(R.id.url);
 
@@ -25,6 +32,37 @@ public class MainActivity extends AppCompatActivity {
         web.setWebViewClient(new WebViewClient());
         web.getSettings().setJavaScriptEnabled(true);
 
-        web.loadUrl("https://ainoal.github.io/portfolio_website/");
+        web.loadUrl("http://www.google.fi");
+
+
+        /*url.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    System.out.println(url.toString());
+                    web.loadUrl(url.toString());
+                    handled = true;
+                }
+                return handled;
+            }
+        });*/
+
+        gotoButton.setOnClickListener(v -> {
+            currentSite = "https://" + url.getText().toString();
+            System.out.println(currentSite);
+
+
+            if (currentSite == null || currentSite.equals("https://")) {
+                System.out.println("No address given");
+
+            } else {
+                web.loadUrl(currentSite);
+            }
+        });
+
+        refresh.setOnClickListener(v -> {
+            web.loadUrl(currentSite);
+        });
     }
 }
